6620a95074adce65219618fa172e194d15be8caa
unknown <kumar.hrk27@gmail.com>
2022-07-04 02:44:25 +0530

    changed port

--- app.js
+++ app.js
@@ -135,6 +135,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.POST || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
